433cb9a03f45eb964054a1c226df8ea0ffb8321a
jaydeep7 <jaydeep7karena7@gmail.com>
2017-06-30 23:39:43 +0530

    Trying to return Item from Products Page Partial View for adding to Cart

--- LearnAspNetMVC/Controllers/CartController.cs
+++ LearnAspNetMVC/Controllers/CartController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearnAspNetMVC.Models;
 
 namespace LearnAspNetMVC.Controllers
 {
     public class CartController : Controller
     {
+
+        List<CartLine> cartLine;
+
+        public CartController()
+        {
+            cartLine = new List<CartLine>();
+        }
+
         // GET: Cart
-        public ActionResult AddToCart(int id, string returnUrl)
+        public ActionResult AddToCart(Item item, int id, string returnUrl)
         {
             return Redirect(returnUrl);
         }
+    }
+
+    public class CartLine
+    {
+        public Item item { get; set; }
+        public int quantity { get; set; }
     }
 }
